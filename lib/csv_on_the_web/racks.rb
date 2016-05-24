e563042e005628c9b3fd5673a329c8ad9194a2b4
pikesley <github@orgraphone.org>
2016-05-24 15:58:20 +0100

    This was just getting in the way

--- lib/csv_on_the_web/racks.rb
+++ lib/csv_on_the_web/racks.rb
@@ -1,25 +1,6 @@
-require 'rack/conneg'
-
 module CsvOnTheWeb
   class App < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'views'
-
-    use Rack::Conneg do |conneg|
-      conneg.set :accept_all_extensions, true
-      conneg.set :fallback, :html
-      conneg.ignore_contents_of 'public'
-      conneg.provide [
-        :html,
-        :csv,
-        :json
-      ]
-    end
-
-    before do
-      if negotiated?
-        content_type negotiated_type
-      end
-    end
   end
 end
